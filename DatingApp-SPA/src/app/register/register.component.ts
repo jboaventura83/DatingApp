d1d7d7013c1744dbbf1cde427f4e9e2734bc758e
jboaventura83@gmail.com <jbnascimento@sp.gov.br>
2019-05-27 17:58:59 -0300

    reactive forms 1

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,18 +12,26 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required)
+    });
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
+   /* this.authService.register(this.model).subscribe(() => {
       this.alertify.success('registrado com sucesso');
     }, error => {
       this.alertify.error(error);
-    });
+    });*/
+    console.log(this.registerForm.value);
   }
 
   cancel() {
